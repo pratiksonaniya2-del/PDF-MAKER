21da0d94946d6618b193adb1d60321a367762396
pratiksonaniya2-del <pratiksonaniya2@gmail.com>
2026-07-23 23:19:40 +0530

    small changes

--- frontend/src/components/common/ThemeToggle.jsx
+++ frontend/src/components/common/ThemeToggle.jsx
@@ -5,7 +5,6 @@ function ThemeToggle() {
     <button
       className="h-10 w-10 rounded-xl border border-gray-200 dark:border-zinc-700 flex items-center justify-center hover:bg-gray-100 dark:hover:bg-zinc-800 transition"
     >
-      <Moon size={18} />
     </button>
   );
 }
